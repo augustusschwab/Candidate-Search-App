a790424c900e9ba38329c285f72e5ec9592eb491
Gus <gus@augustuss-mbp-2.lan>
2025-03-03 06:57:13 -0700

    Added CSS for the candidate card on the candidate search page. Updated nav bar styling.

--- src/components/CandidateCard.tsx
+++ src/components/CandidateCard.tsx
@@ -8,24 +8,24 @@ type CandidateCardProps = {
 const CandidateCard: React.FC<CandidateCardProps> = ({candidate}) => {
     return(
         <section className='candidateCard'>
-            <figure>
-                <img src={`${candidate.Avatar}`} alt={`${candidate.Username}`} />
-            </figure>
-            <article className='details'>
-            <h2>{candidate.Name}</h2>
-            <p>
-              <strong>Username:</strong> <a href={`${candidate.html_url}`}>{candidate.Username}</a>
-            </p>
-            <p>
-              <strong>Location:</strong> {candidate.Location}
-            </p>
-            <p>
-              <strong>Email:</strong> {candidate.Email}
-            </p>
-            <p>
-              <strong>Company:</strong> {candidate.Company}
-            </p>
-          </article>
+            <img src={`${candidate.Avatar}`} alt={`${candidate.Username}`} />
+            <article>
+                <h2>{candidate.Name}</h2>
+                <div className="details">
+                    <p>
+                    <strong>Username:</strong> <a href={`${candidate.html_url}`}>{candidate.Username}</a>
+                    </p>
+                    <p>
+                    <strong>Location:</strong> {candidate.Location}
+                    </p>
+                    <p>
+                    <strong>Email:</strong> {candidate.Email}
+                    </p>
+                    <p>
+                    <strong>Company:</strong> {candidate.Company}
+                    </p>
+                </div>
+            </article>
         </section>
     )
 }
